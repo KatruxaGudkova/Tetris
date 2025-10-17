19649a12dbe8ed636fb3974deeb8c67524c98790
katruxagudkova <135836664+KatruxaGudkova@users.noreply.github.com>
2025-10-17 12:28:15 +0000

    Создала необходимые папки. Создала компонент Game, попробовала предать row и columns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./styles.css";
+import Game from "./components/Game.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Game rows={20} columns={10} />
     </div>
   );
 }
