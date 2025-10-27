5e79b4fd51f7e5dbc7c5161fbfbf579627a1f7cd
katruxagudkova <135836664+KatruxaGudkova@users.noreply.github.com>
2025-10-27 11:24:03 +0000

    Добавили вывод уровня, линий уровня и количество поинтов.

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,13 +1,19 @@
 import "./Tetris.css";
+
 import Board from "./Board.js";
+import GameStats from "./GameStats.js";
+
 import { useBoard } from "../hooks/useBoard.js";
+import { useGameStats } from "../hooks/useGameStats.js";
 
 const Tetris = ({ rows, columns, setGameOver }) => {
+  const [gameStats, addLinesCleared] = useGameStats();
   const [board, setBoard] = useBoard({ rows, columns });
 
   return (
     <div className="Tetris">
       <Board board={board} />
+      <GameStats gameStats={gameStats} />
     </div>
   );
 };
