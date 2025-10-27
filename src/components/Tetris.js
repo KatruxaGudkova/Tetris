1ec5dbc08d569b36cbf9a2b186b2fdf0e865e381
katruxagudkova <135836664+KatruxaGudkova@users.noreply.github.com>
2025-10-27 12:22:47 +0000

    Создали компоненту предпростмотра. Создали тетромино - 7 фигур I, J, L, O, S, T, Z разных цветов, которые вызываются рандомно с помощью ф-ции randomTetromino

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -2,18 +2,22 @@ import "./Tetris.css";
 
 import Board from "./Board.js";
 import GameStats from "./GameStats.js";
+import Previews from "./Previews.js";
 
 import { useBoard } from "../hooks/useBoard.js";
 import { useGameStats } from "../hooks/useGameStats.js";
+import { usePlayer } from "../hooks/usePlayer.js";
 
 const Tetris = ({ rows, columns, setGameOver }) => {
   const [gameStats, addLinesCleared] = useGameStats();
   const [board, setBoard] = useBoard({ rows, columns });
+  const [player, setPlayer, resetPlayer] = usePlayer();
 
   return (
     <div className="Tetris">
       <Board board={board} />
       <GameStats gameStats={gameStats} />
+      <Previews tetrominoes={player.tetrominoes} />
     </div>
   );
 };
