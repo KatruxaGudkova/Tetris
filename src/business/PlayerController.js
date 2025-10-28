f67e280a28b5e9c7391dcfc0da507093a690bd43
katruxagudkova <135836664+KatruxaGudkova@users.noreply.github.com>
2025-10-28 12:44:44 +0000

    Добавили перемещение фигуры вправо/влево/вниз

--- src/business/PlayerController.js
+++ src/business/PlayerController.js
@@ -5,7 +5,7 @@ import { Action } from "./Input.js";
 const attemptRotation = ({ board, player, setPlayer }) => {
   const shape = rotate({
     piece: player.tetromino.shape,
-    direction: 1,
+    direction: 1, // если поставить -1, вращение в другую сторону
   });
 
   const position = player.position;
@@ -26,6 +26,68 @@ const attemptRotation = ({ board, player, setPlayer }) => {
   }
 };
 
+export const movePlayer = ({ delta, position, shape, board }) => {
+  const desiredNextPosition = {
+    row: position.row + delta.row,
+    column: position.column + delta.column,
+  };
+
+  const collided = hasCollision({
+    board,
+    position: desiredNextPosition,
+    shape,
+  });
+
+  const isOnBoard = isWithinBoard({
+    board,
+    position: desiredNextPosition,
+    shape,
+  });
+
+  const preventMove = !isOnBoard || (isOnBoard && collided);
+  const nextPosition = preventMove ? position : desiredNextPosition;
+
+  const isMovingDown = delta.row > 0;
+  const isHit = isMovingDown && (collided || !isOnBoard);
+
+  return { collided: isHit, nextPosition };
+};
+
+const attemptMovement = ({ board, action, player, setPlayer, setGameOver }) => {
+  const delta = { row: 0, column: 0 };
+  let isFastDropping = false;
+
+  if (action === Action.FastDrop) {
+    isFastDropping = true;
+  } else if (action === Action.SlowDrop) {
+    delta.row += 1;
+  } else if (action === Action.Left) {
+    delta.column -= 1;
+  } else if (action === Action.Right) {
+    delta.column += 1;
+  }
+
+  const { collided, nextPosition } = movePlayer({
+    delta,
+    position: player.position,
+    shape: player.tetromino.shape,
+    board,
+  });
+
+  // Did we collide immediately? If so, game over, man!
+  const isGameOver = collided && player.position.row === 0;
+  if (isGameOver) {
+    setGameOver(isGameOver);
+  }
+
+  setPlayer({
+    ...player,
+    collided,
+    isFastDropping,
+    position: nextPosition,
+  });
+};
+
 export const playerController = ({
   action,
   board,
@@ -37,7 +99,7 @@ export const playerController = ({
 
   if (action === Action.Rotate) {
     attemptRotation({ board, player, setPlayer });
-    //   } else {
-    //     attemptMovement({ board, player, setPlayer, action, setGameOver });
+  } else {
+    attemptMovement({ board, player, setPlayer, action, setGameOver });
   }
 };
